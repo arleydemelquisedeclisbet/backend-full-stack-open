7a16a69b1b87bc4f0a9a3664d8a9e6a8be8b6058
arleydemelquisedeclisbet <94724221+arleydemelquisedeclisbet@users.noreply.github.com>
2024-04-13 01:22:37 -0500

    3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from 'express'
-import datos from './datos.js'
-
+let datos = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
 const app = express()
 
 app.get('/api/persons', (_req, res) => {
@@ -14,6 +34,13 @@ app.get('/api/persons/:id', (req, res) => {
     return personFound
         ? res.send(personFound)
         : res.status(404).send('Not found')
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    datos = datos.filter(p => p.id !== +id)
+
+    res.status(204).end()
 })
 
 app.get('/info', (_req, res) => {
