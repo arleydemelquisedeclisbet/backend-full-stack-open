20f5e210c64ce55672317d1a1c3937d6725b2baf
arleydemelquisedeclisbet <94724221+arleydemelquisedeclisbet@users.noreply.github.com>
2024-04-13 01:10:37 -0500

    3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/api/persons', (_req, res) => {
     res.send(datos)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    const personFound = datos.find(p => p.id === +id)
+
+    return personFound
+        ? res.send(personFound)
+        : res.status(404).send('Not found')
+})
+
 app.get('/info', (_req, res) => {
     const numberOfRegisters = datos.length
     const message = `Phonebook has info for ${numberOfRegisters} people`
