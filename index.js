e170da6bb89963459a945a59782f01b3226812e6
arleydemelquisedeclisbet <94724221+arleydemelquisedeclisbet@users.noreply.github.com>
2024-04-13 00:58:51 -0500

    3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/api/persons', (_req, res) => {
     res.send(datos)
 })
 
+app.get('/info', (_req, res) => {
+    const numberOfRegisters = datos.length
+    const message = `Phonebook has info for ${numberOfRegisters} people`
+    const dateRequest = new Date()
+    res.send(`${message} <br/><br/> ${dateRequest}`)
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
